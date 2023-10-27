a399595b9a41411ff3d66ff07ce00cff7ea72650
willdjm <will_djm@hotmail.com>
2023-10-26 22:00:16 -0300

    first commit

--- src/app/components/Treinador.tsx
+++ src/app/components/Treinador.tsx
@@ -3,15 +3,15 @@ export function Treinador() {
 return (
 
 <section className="">
-<div id="sobre" className="max-w-full md:h-screen mx-auto flex ">
+<div id="sobre" className="max-w-full lg:h-screen mx-auto flex ">
 
-<div className="md:flex grid ">
-<div className="md:w-1/3 md:h-screen lg:block">
+<div className="lg:flex grid ">
+<div className="lg:w-1/3 lg:h-screen lg:flex">
 <picture>
-                    <img src="./Rectangle40.svg" alt=""  className="object-cover w-full h-full"/>
+                    <img src="./Rectangle40.svg" alt=""  className="lg:object-cover lg:w-full lg:h-full"/>
                   </picture>
 </div>
-<div className="md:w-1/3  flex-col flex items-start justify-center gap-6 pl-16 py-6">
+<div className="lg:w-1/3  flex-col flex items-start justify-center gap-6 lg:pl-16 p-5 py-6">
   <h2 className="text-2xl uppercase ">
   Treinador
   </h2>
@@ -31,11 +31,11 @@ Cref 14877i-G/SP
   <p>
   Atleta profissional
   </p>
-  <p className=" max-w-sm text-left">
+  <p className="lg:max-w-sm text-left">
   Criador da Equipe Águia, cujo intuito era reunir amigos apaixonados pela corrida e proporcionar maior acessibilidade às inscrições para participação em corridas de rua.
   </p>
 </div>
-<div className="md:w-1/3 md:h-screen grid  grid-cols-2 ">
+<div className="lg:w-1/3 lg:h-screen grid  grid-cols-2 ">
 <picture>
                     <img src="./Rectangle41.svg" alt=""  className="object-cover w-full h-full"/>
                   </picture>
