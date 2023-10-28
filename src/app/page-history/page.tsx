7c8dbb68d4cc9fb57635e7af879f753633ac3017
willdjm <will_djm@hotmail.com>
2023-10-28 15:30:13 -0300

    first commit

--- src/app/page-history/page.tsx
+++ src/app/page-history/page.tsx
@@ -1,32 +1,24 @@
 "use client";
 
 import { BannerPageHistory } from "../components/BannerPageHistory";
-import { Desenvolvimento } from "../components/Desenvolvimento";
 import { History } from "../components/History";
 import { Missao } from "../components/Missao";
 import { NavBar } from "../components/NavBar";
-import { Satisfacao } from "../components/Satisfacao";
 import { Siga } from "../components/Siga";
 import { Valores } from "../components/Valores";
 import { Contact } from "../components/contact";
 
 
 export default function PageHistory() {
-  return (
-    <main>
-
-      
-<NavBar/>
-<BannerPageHistory/>
-<History/>
-<Missao/>
-<Valores/>
-<Satisfacao/>
-<Desenvolvimento/>
-<Siga/>
-<Contact/>
-
-
-    </main>
-  )
+return (
+<main>
+  <NavBar />
+  <BannerPageHistory />
+  <History />
+  <Missao />
+  <Valores />
+  <Siga />
+  <Contact />
+</main>
+)
 }
